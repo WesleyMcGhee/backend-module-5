ffd5f34889e401a946a027b8c51d2ed19c2ef82d
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-07-09 10:49:23 -0500

    done

---
+++ client/index.js
@@ -0,0 +1,55 @@
+const cryptoBtn = document.querySelector('#cryptoBtn');
+const cryptoText = document.querySelector('#cryptoText');
+const cryptoForm = document.querySelector('#cryptoPrice');
+const cryptoDiv = document.querySelector('#cryptoDiv');
+const cryptoH1 = document.querySelector('#cryptoH1');
+const pokeBtn = document.querySelector('#pokeBtn');
+
+document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+  };
+  document.getElementById("fortuneButton").onclick = function () {
+    axios.get('http://localhost:4000/api/fortune')
+    .then(function (response){
+      const data = response.data;
+      alert(data);
+    })
+  }
+  function randPoke (){
+    let id = Math.floor((Math.random()) * 800) + 1;
+    console.log(id);
+    return id;
+  }
+  const randomPokeGen = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${randPoke()}`).then((res) => {
+      let image = res.data.sprites.front_default;   
+      const pokeDiv = document.querySelector('#pokeDiv');
+      let newImg = document.createElement('img');
+      let newP = document.createElement('p');
+      delete newImg;
+      newImg.src = image;
+      pokeDiv.appendChild(newImg);
+      pokeDiv.appendChild(newP).textContent = res.data.name;
+    })
+  }
+  const getCryptoPrice = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.cryptonator.com/api/ticker/${cryptoText.value.toLowerCase()}-usd`).then((res) =>{
+      cryptoH1.textContent = `$${res.data.ticker.price}`;
+      cryptoText.value = "";
+    })
+  }
+  const chuck = document.querySelector('#chuck');
+  const chuckJoke = () => {
+      axios.get('https://api.chucknorris.io/jokes/random').then((res) => {
+          alert(res.data.value);
+      })
+  }
+  
+  cryptoForm.addEventListener('submit', getCryptoPrice);
+  pokeBtn.addEventListener('click', randomPokeGen);
+  chuck.addEventListener('click', chuckJoke);
---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+let fortuneId = 5;
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+const fortune = [
+  'A beautiful, smart, and loving person will be coming into your life.',
+  'A dubious friend may be an enemy in camouflage.',
+  'A faithful friend is a strong defense.',
+  'A feather in the hand is better than a bird in the air.',
+  'A fresh start will put you on your way.'
+]
+
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+app.get('/api/fortune', (req, res) => {
+  
+
+  let randomIndex = Math.floor(Math.random() * fortune.length);
+  let randomCompliment = fortune[randomIndex];
+
+  res.status(200).send(randomCompliment);
+})
+
+
+app.listen(4000, () => console.log("Server running on 4000"));
